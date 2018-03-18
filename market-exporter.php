c90cc2904a35ec8bab1c0eb10dde1c2bee69632a
Anton Vanyukov <a.vanyukov@vcore.ru>
2018-03-18 19:58:17 +0300

    Update changelog. Update select2 library to 4.0.5

--- market-exporter.php
+++ market-exporter.php
@@ -8,7 +8,7 @@
  * Plugin Name: Market Exporter
  * Plugin URI: https://github.com/av3nger/market-exporter/
  * Description: Market Exporter provides a way to export products from WooCommerce installations into a YML file for use in Yandex Market.
- * Version: 1.0.3-beta.1
+ * Version: 1.0.3
  * Author: Anton Vanyukov
  * Author URI: http://www.vanyukov.su
  * License: GPLv2 or later
