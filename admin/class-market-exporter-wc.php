655eb025b01e2b23c310eba5a06cff4a258cb044
Anton Vanyukov <anton@incsub.com>
2019-04-14 19:56:15 +0300

    Fix merge conflicts

--- admin/class-market-exporter-wc.php
+++ admin/class-market-exporter-wc.php
@@ -273,11 +273,6 @@ class ME_WC {
 			}
 
 			// We use a separate variable for offer because we will be rewriting it for variable products.
-            
-            if (apply_filters('market_exporter_exclude_post', false, $query->post, $product)) {
-                continue;
-            }
-
 			$offer = $product;
 
 			/*
